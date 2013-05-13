b60152a72e9611e006ecea53fd7854526d457b46
AMPivovarov <AMPivovarov@gmail.com>
2013-05-13 15:58:44 +0400

    Pre-release

--- stfw.py
+++ stfw.py
@@ -8,14 +8,21 @@ import zlib
 import json
 import subprocess, os
 import argparse
+import webbrowser
 
 parser = argparse.ArgumentParser(description='StackOverflow helper.')
-parser.add_argument('--verbose', dest='verbose', action='store_const',
+parser.add_argument('-v', '--verbose', dest='verbose', action='store_const',
                    const=1, default=0,
                    help='show verbose output')
+parser.add_argument('-u', '--url-open', dest='url_open', action='store_const',
+                   const=1, default=0,
+                   help='use system open command for url')
+parser.add_argument('-i', '--open-with', dest='open_with', action='store',
+                   default='',
+                   help='use custom open command for url')
 args = parser.parse_args()
 
-url = "https://api.stackexchange.com/2.1/search/advanced?order=desc&sort=relevance&site=stackoverflow&q="
+request_url = "https://api.stackexchange.com/2.1/search/advanced?order=desc&sort=relevance&site=stackoverflow&q="
 
 match_error = re.compile("^([^:]*):([^:]*):([^:]*): (warning|error|fatal error): (.*)$")
 match_tagged_message = re.compile("^(.*) (\[[^ ]*\])$")
@@ -41,8 +48,8 @@ while 1:
     if not errorline:
         break
     
-    match = match_error.search(errorline)
-    if match:
+    data = match_error.search(errorline)
+    if data:
         try:
             codeline = sys.stdin.readline()
             if args.verbose:
@@ -50,14 +57,12 @@ while 1:
         except KeyboardInterrupt:
             break
 
-        data = match_error.search(errorline)
-
         filename = data.group(1)
         line = data.group(2)
         row = data.group(3)
         level = data.group(4)
-        if (match_tagged_message.match(data.group(5))):
-            data2 = match_tagged_message.search(data.group(5))
+        data2 = match_tagged_message.search(data.group(5))
+        if (data2):
             message = data2.group(1)
             tag = data2.group(2)
         else:    
@@ -65,44 +70,48 @@ while 1:
             tag = ""
         
         if message not in messages:
-           messages.update({message:{"errorline":errorline, "codeline":[], "filename":[], "line":[], "row":[], "level":level, "message":message, "tag":tag}})
+           messages.update({message:{"codeline":[], "filename":[], "line":[], "row":[], "level":level, "message":message, "tag":tag}})
 
         messages[message]["codeline"].append(codeline)
         messages[message]["filename"].append(filename)
         messages[message]["line"].append(line)
         messages[message]["row"].append(row)
+if args.verbose:
+    print
 
 # Display user menu
 sys.stdin = open('/dev/tty')
 
-if args.verbose:
-    print
-i = 1
-for message,m in messages.items():
-    print '{0} {1}: {2}'.format(paint(i, bcolors.HEADER), m['level'], message)
-    i = i + 1
-print "==> Enter message number" 
-print "==> --------------------"
+if len(messages.items()) == 0:
+    sys.exit(0)
+if len(messages.items()) == 1:
+    num = 1
+else:
+    i = 1
+    for message,m in messages.items():
+        print '{0} {1}: {2}'.format(paint(i, bcolors.HEADER), m['level'], message)
+        i = i + 1
+    print "==> Enter message number" 
+    print "==> --------------------"
 
-try:
-    num = int(input('==> '))
-except:
-    sys.exit(0)
-if num > len(messages) or num <= 0:
-    sys.exit(0)
-print
+    try:
+        num = int(input('==> '))
+    except:
+        sys.exit(0)
+    if num > len(messages) or num <= 0:
+        sys.exit(0)
+    print
 
 message = messages[messages.keys()[num-1]]
 
 # Hardwork
 opener = urllib2.build_opener()
-req = url + (urllib2.quote(message['message']))
+req = request_url + (urllib2.quote(message['message']))
 data = opener.open(req).read()
 jsondata = json.loads(zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data))
 
-
 # Display user menu
-print "Links: " + message['message']
+print "Request: " + message['message']
 print '{0} {1}'.format(paint(0, bcolors.HEADER), 'Google for me')
 j = 1
 for answer in jsondata['items']:
@@ -125,10 +134,20 @@ if num == 0:
 else :
     link = jsondata['items'][num-1]['link']
 
+# open URL
 print "Opening " + link
-if sys.platform.startswith('darwin'):
-    subprocess.call(('open', link))
-elif os.name == 'nt':
-    os.startfile(link)
-elif os.name == 'posix':
-    subprocess.call(('xdg-open', link))
+
+if args.open_with:
+    subprocess.call((args.open_with, link))
+    sys.exit(0)
+
+if args.url_open:
+    if sys.platform.startswith('darwin'):
+        subprocess.call(('open', link))
+    elif os.name == 'nt':
+        os.startfile(link)
+    elif os.name == 'posix':
+        subprocess.call(('xdg-open', link))
+    sys.exit(0)
+
+webbrowser.open(link.replace(u'’','\'').replace(u'‘','\''), new=0)
