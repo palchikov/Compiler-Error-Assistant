054dde8a8e8e4feec1cf26ee86cc7eed9ed1ede0
Alexander Shabalin <shabalyn.a@gmail.com>
2013-05-14 15:03:15 +0400

    Wrappers improved

---
+++ wrapper.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+executable=$1
+shift
+script=$1
+shift
+
+function usage() {
+   echo "Wrapper usage: $script [stfw flags] -- [gcc flags]"
+   echo -n "Utility "
+   ./stfw.py --help
+}
+
+STFW_FLAGS=
+
+while [ \( "$1" != '--' \) -a -n "$1" ]; do
+   if [ \( $1 == '--help' \) -o \( $1 == '-h' \) ]; then
+      usage
+      exit 0
+   fi
+   STFW_FLAGS=$STFW_FLAGS' '$1
+   shift
+done
+
+if [ -z $1 ]; then
+   usage
+   exit 0
+fi
+
+shift
+
+$executable $* 2>&1 | ./stfw.py $STFW_FLAGS
